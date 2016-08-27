5176fd874cb65f055aa8c132fbcb4087d6700f33
Jacob Muhle <jmuhle.gc@gmail.com>
2016-08-27 17:51:32 -0600

    original functions with some cleanup on the display counts

--- javascript.js
+++ javascript.js
@@ -7,23 +7,64 @@ var timeSigTop = 3;
 var beatPerMeasure;
 var timeSigBot = 0;
 var beatSound = 0;
-var sound;
 var beatCount;
 var subdiv = 0;
 var subdivide;
 var speed;
 
-function bottomTime(index) {
-  	if (index == 1 || index == 2) {
-  		timeSigBot = index;
-  		document.getElementById("meter").innerHTML = "8th =";
-    	document.getElementById("subdivision").setAttribute("disabled","disabled");
+function onChangeSignature() {
+	document.getElementById("subdivision4").style.display = "none";
+	document.getElementById("subdivision85").style.display = "none";
+	document.getElementById("subdivision87").style.display = "none";
+	if (document.getElementById("timeSigBot").selectedIndex === 1) {
+		document.getElementById("meter").innerHTML = "8th =";
+		if (document.getElementById("timeSigTop").selectedIndex === 4) {
+    		document.getElementById("subdivision85").style.display = "inline";
+		} else if (document.getElementById("timeSigTop").selectedIndex === 6) {
+    		document.getElementById("subdivision87").style.display = "inline";
+		}
 	} else {
-		timeSigBot = index;
 		document.getElementById("meter").innerHTML = "4th =";
-    	document.getElementById("subdivision").removeAttribute("disabled");
+		document.getElementById("subdivision4").style.display = "inline";
 	}
 }
+
+
+// function topTime(index) {
+// 	if (index == 4) {
+// 		document.getElementById("five1").removeAttribute("disabled");
+// 		document.getElementById("five1").setAttribute("selected","selected");
+// 		document.getElementById("five2").removeAttribute("disabled");
+// 		document.getElementById("seven1").setAttribute("disabled", "disabled");
+// 		document.getElementById("seven2").setAttribute("disabled", "disabled");
+// 		document.getElementById("seven3").setAttribute("disabled", "disabled");
+// 		document.getElementById("seven4").setAttribute("disabled", "disabled");
+// 	} else if (index == 6) {
+// 		document.getElementById("five1").setAttribute("disabled", "disabled");
+// 		document.getElementById("five2").setAttribute("disabled", "disabled");
+// 		document.getElementById("seven1").removeAttribute("disabled");
+// 		document.getElementById("seven1").setAttribute("selected","selected");
+// 		document.getElementById("seven2").removeAttribute("disabled");
+// 		document.getElementById("seven3").removeAttribute("disabled");
+// 		document.getElementById("seven4").removeAttribute("disabled");
+// 	}
+// }
+
+// function bottomTime(index) {
+//   	if (index == 1) {
+//   		timeSigBot = index;
+//   		document.getElementById("meter").innerHTML = "8th =";
+//     	document.getElementById("subdivision4").style.display = "none";
+//     	document.getElementById("subdivision8").style.display = "inline";
+// 	} else {
+// 		timeSigBot = index;
+// 		document.getElementById("meter").innerHTML = "4th =";
+//     	document.getElementById("subdivision8").style.display = "none";
+//     	document.getElementById("subdivision4").style.display = "inline";
+// 	}
+// }
+
+
 
 
 function createOsc4(subdivide, beatPerMeasure) {
@@ -36,49 +77,50 @@ function createOsc4(subdivide, beatPerMeasure) {
     gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
 
     if (beatSound === 4) {
-		if (beatCount % subdivide === 0) {
+    	debugger;
+		if (beatCount % beatPerMeasure === 0) {
 			document.getElementById("overlay").innerHTML = "1";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
-		} else if (beatCount % subdivide === subdivide * (1 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (1/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "2";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
-		} else if (beatCount % subdivide === subdivide * (2 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (2/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "3";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
-		} else if (beatCount % subdivide === subdivide * (3 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (3/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "4";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
-		} else if (beatCount % subdivide === subdivide * (4 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (4/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "5";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
-		} else if (beatCount % subdivide === subdivide * (5 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (5/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "6";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
-		} else if (beatCount % subdivide === subdivide * (6 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (6/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "7";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
-		} else if (beatCount % subdivide === subdivide * (7 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (7/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "8";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
-		} else if (beatCount % subdivide === subdivide * (8 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (8/ beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "9";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
-		} else if (beatCount % subdivide === subdivide * (9 / beatPerMeasure)) {
+		} else if (beatCount % beatPerMeasure === subdivide * (9 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "10";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
 		} else if (beatCount % subdivide === subdivide * (10 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "11";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
 		} else if (beatCount % subdivide === subdivide * (11 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "12";
@@ -86,7 +128,7 @@ function createOsc4(subdivide, beatPerMeasure) {
 			osc.type = "sawtooth";
 		} else if (beatCount % subdivide === subdivide * (12 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "13";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
 		} else if (beatCount % subdivide === subdivide * (13 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "14";
@@ -94,52 +136,25 @@ function createOsc4(subdivide, beatPerMeasure) {
 			osc.type = "sawtooth";
 		} else if (beatCount % subdivide === subdivide * (14 / beatPerMeasure)) {
 			document.getElementById("overlay").innerHTML = "15";
-			osc.frequency.value = 110;
+			osc.frequency.value = 120;
 			osc.type = "sine";
 		} else {
 			osc.frequency.value = 880;
 			osc.type = "triangle";
 		}
+
 	} else {
+		debugger;
+		document.getElementById("overlay").innerHTML = Math.floor(beatCount % subdivide) + 1;
+
 		if (beatSound === 0) {
-			sound = "triangle";
+			osc.type = "triangle";
 		} else if (beatSound === 1) {
-			sound = "sine";
+			osc.type = "sine";
 		} else if (beatSound === 2) {
-			sound = "square";
+			osc.type = "square";
 		} else if (beatSound === 3) {
-			sound = "sawtooth";
-		}
-		if (beatCount % subdivide === 0) {
-			document.getElementById("overlay").innerHTML = "1";
-		} else if (beatCount % subdivide === subdivide * (1 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "2";
-		} else if (beatCount % subdivide === subdivide * (2 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "3";
-		} else if (beatCount % subdivide === subdivide * (3 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "4";
-		} else if (beatCount % subdivide === subdivide * (4 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "5";
-		} else if (beatCount % subdivide === subdivide * (5 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "6";
-		} else if (beatCount % subdivide === subdivide * (6 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "7";
-		} else if (beatCount % subdivide === subdivide * (7 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "8";
-		} else if (beatCount % subdivide === subdivide * (8 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "9";
-		} else if (beatCount % subdivide === subdivide * (9 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "10";
-		} else if (beatCount % subdivide === subdivide * (10 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "11";
-		} else if (beatCount % subdivide === subdivide * (11 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "12";
-		} else if (beatCount % subdivide === subdivide * (12 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "13";
-		} else if (beatCount % subdivide === subdivide * (13 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "14";
-		} else if (beatCount % subdivide === subdivide * (14 / beatPerMeasure)) {
-			document.getElementById("overlay").innerHTML = "15";
+			osc.type = "sawtooth";
 		}
 
 		if (beatCount % subdivide === 0) {
@@ -148,7 +163,6 @@ function createOsc4(subdivide, beatPerMeasure) {
 			osc.frequency.value = 220;
 		}
 
-		osc.type = sound;
 	}
 
 	osc.connect(gain);
@@ -160,7 +174,11 @@ function createOsc4(subdivide, beatPerMeasure) {
         gain.disconnect(audio.destination);
     }, 50);
     beatCount += 1;
-}
+} // end createOsc4()
+
+
+
+
 
 function createOsc8(beatPerMeasure, timeSigBot) {
 	var gain = audio.createGain();
@@ -171,97 +189,42 @@ function createOsc8(beatPerMeasure, timeSigBot) {
     gain.gain.linearRampToValueAtTime(1, audio.currentTime + 1 / 1000);
     gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
 
+	document.getElementById("overlay").innerHTML = (beatCount % beatPerMeasure) + 1;
 
-	if (beatSound === 4 && timeSigBot === 1) {
+	if (beatSound === 4) {
+
 		if (beatCount % beatPerMeasure === 0) {
-			document.getElementById("overlay").innerHTML = "1";
-			osc.frequency.value = 110;
-			osc.type = "sine";
-		} else if (beatCount % beatPerMeasure === 1) {
-			document.getElementById("overlay").innerHTML = "2";
-			osc.frequency.value = 220;
-			osc.type = "sawtooth";
-		} else if (beatCount % beatPerMeasure === 2) {
-			document.getElementById("overlay").innerHTML = "3";
 			osc.frequency.value = 110;
 			osc.type = "sine";
 		} else if (beatCount % beatPerMeasure === 3) {
-			document.getElementById("overlay").innerHTML = "4";
 			osc.frequency.value = 220;
 			osc.type = "sawtooth";
-		} else if (beatCount % beatPerMeasure === 4) {
-			document.getElementById("overlay").innerHTML = "5";
-			osc.frequency.value = 110;
-			osc.type = "sine";
-		} else if (beatCount % beatPerMeasure === 5) {
-			document.getElementById("overlay").innerHTML = "6";
-			osc.frequency.value = 220;
-			osc.type = "sawtooth";
-		} else if (beatCount % beatPerMeasure === 6) {
-			document.getElementById("overlay").innerHTML = "7";
-			osc.frequency.value = 110;
-			osc.type = "sine";
-		} else if (beatCount % beatPerMeasure === 7) {
-			document.getElementById("overlay").innerHTML = "8";
-			osc.frequency.value = 220;
-			osc.type = "sawtooth";
-		} else if (beatCount % beatPerMeasure === 8) {
-			document.getElementById("overlay").innerHTML = "9";
-			osc.frequency.value = 110;
-			osc.type = "sine";
 		} else {
 			osc.frequency.value = 880;
 			osc.type = "triangle";
 		}
+
 	} else {
+
 		if (beatSound === 0) {
-			sound = "triangle";
+			osc.type = "triangle";
 		} else if (beatSound === 1) {
-			sound = "sine";
+			osc.type = "sine";
 		} else if (beatSound === 2) {
-			sound = "square";
+			osc.type = "square";
 		} else if (beatSound === 3) {
-			sound = "sawtooth";
-		}
-		if (beatCount % subdivide === 0) {
-			document.getElementById("overlay").innerHTML = "1";
-		} else if (beatCount % beatPerMeasure === 1) {
-			document.getElementById("overlay").innerHTML = "2";
-		} else if (beatCount % beatPerMeasure === 2) {
-			document.getElementById("overlay").innerHTML = "3";
-		} else if (beatCount % beatPerMeasure === 3) {
-			document.getElementById("overlay").innerHTML = "4";
-		} else if (beatCount % beatPerMeasure === 4) {
-			document.getElementById("overlay").innerHTML = "5";
-		} else if (beatCount % beatPerMeasure === 5) {
-			document.getElementById("overlay").innerHTML = "6";
-		} else if (beatCount % beatPerMeasure === 6) {
-			document.getElementById("overlay").innerHTML = "7";
-		} else if (beatCount % beatPerMeasure === 7) {
-			document.getElementById("overlay").innerHTML = "8";
-		} else if (beatCount % beatPerMeasure === 8) {
-			document.getElementById("overlay").innerHTML = "9";
-		} else if (beatCount % beatPerMeasure === 9) {
-			document.getElementById("overlay").innerHTML = "10";
-		} else if (beatCount % beatPerMeasure === 10) {
-			document.getElementById("overlay").innerHTML = "11";
-		} else if (beatCount % beatPerMeasure === 11) {
-			document.getElementById("overlay").innerHTML = "12";
-		} else if (beatCount % beatPerMeasure === 12) {
-			document.getElementById("overlay").innerHTML = "13";
-		} else if (beatCount % beatPerMeasure === 13) {
-			document.getElementById("overlay").innerHTML = "14";
-		} else if (beatCount % beatPerMeasure === 14) {
-			document.getElementById("overlay").innerHTML = "15";
+			osc.type = "sawtooth";
 		}
 
-		if (beatCount % subdivide === 0) {
+		if (beatCount % beatPerMeasure === 0) {
 			osc.frequency.value = 440;
+		} else if (beatCount % beatPerMeasure === 3) {
+			osc.frequency.value = 330;
 		} else {
 			osc.frequency.value = 220;
 		}
+		
 
-		osc.type = sound;
 	}
 
 	osc.connect(gain);
@@ -273,41 +236,14 @@ function createOsc8(beatPerMeasure, timeSigBot) {
        	gain.disconnect(audio.destination);
     }, 50);
     beatCount += 1;
-}
+} // end createOsc8()
+
+
+
 
 function startMetronome() {
 
-	if (timeSigTop === 0) {
-		beatPerMeasure = 1;
-	} else if (timeSigTop === 1) {
-		beatPerMeasure = 2;
-	} else if (timeSigTop === 2) {
-		beatPerMeasure = 3;
-	} else if (timeSigTop === 3) {
-		beatPerMeasure = 4;
-	} else if (timeSigTop === 4) {
-		beatPerMeasure = 5;
-	} else if (timeSigTop === 5) {
-		beatPerMeasure = 6;
-	} else if (timeSigTop === 6) {
-		beatPerMeasure = 7;
-	} else if (timeSigTop === 7) {
-		beatPerMeasure = 8;
-	} else if (timeSigTop === 8) {
-		beatPerMeasure = 9;
-	} else if (timeSigTop === 9) {
-		beatPerMeasure = 10;
-	} else if (timeSigTop === 10) {
-		beatPerMeasure = 11;
-	} else if (timeSigTop === 11) {
-		beatPerMeasure = 12;
-	} else if (timeSigTop === 12) {
-		beatPerMeasure = 13;
-	} else if (timeSigTop === 13) {
-		beatPerMeasure = 14;
-	} else if (timeSigTop === 14) {
-		beatPerMeasure = 15;
-	}
+	beatPerMeasure = (timeSigTop + 1);
 
 	if (timeSigBot === 0) {
 		if (subdiv === 0) {
@@ -336,12 +272,18 @@ function startMetronome() {
 		interval = setInterval(createOsc8, 60000/speed, beatPerMeasure, timeSigBot); 
 
 	}
-}
+} // end startMetronome()
+
+
+
 
 function stopMetronome() {
 	document.getElementById("overlay").innerHTML = "";
 	clearInterval(interval);
 }
+
+
+
 
 function eventSS() {
 	if (started === false) {
@@ -357,7 +299,10 @@ function eventSS() {
 	}
 }
 
+
+
 document.getElementById("eventButton").onclick = eventSS;
+
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         eventSS();
