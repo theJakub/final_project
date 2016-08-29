3d96328a9ee069e49bfab7d9ca02d8f6497c0ae1
Jacob Muhle <Jacob Muhle jmuhle.gc@gmail.com>
2016-08-29 11:25:31 -0600

    commit 4. final 1.0

--- javascript.js
+++ javascript.js
@@ -9,6 +9,12 @@ function onChangeSignature() {															// Switches the subdivision display
 	document.getElementById("subdivision85").style.display = "none";
 	document.getElementById("subdivision87").style.display = "none";
 	document.getElementById("subdivision88").style.display = "none";
+	document.getElementById("subdivision89").style.display = "none";
+	document.getElementById("subdivision810").style.display = "none";
+	document.getElementById("subdivision811").style.display = "none";
+	document.getElementById("subdivision813").style.display = "none";
+	document.getElementById("subdivision814").style.display = "none";
+	document.getElementById("subdivision815").style.display = "none";
 
 	document.getElementById("subdivision8na").style.display = "none";
 
@@ -24,9 +30,25 @@ function onChangeSignature() {															// Switches the subdivision display
 		} else if (document.getElementById("timeSigTop").selectedIndex === 7) {
 			document.getElementById("subdivision88").style.display = "inline";
 
-		}
-
-		else {
+		} else if (document.getElementById("timeSigTop").selectedIndex === 8) {
+			document.getElementById("subdivision89").style.display = "inline";
+		
+		} else if (document.getElementById("timeSigTop").selectedIndex === 9) {
+			document.getElementById("subdivision810").style.display = "inline";
+		
+		} else if (document.getElementById("timeSigTop").selectedIndex === 10) {
+			document.getElementById("subdivision811").style.display = "inline";
+		
+		} else if (document.getElementById("timeSigTop").selectedIndex === 12) {
+			document.getElementById("subdivision813").style.display = "inline";
+		
+		} else if (document.getElementById("timeSigTop").selectedIndex === 13) {
+			document.getElementById("subdivision814").style.display = "inline";
+		
+		} else if (document.getElementById("timeSigTop").selectedIndex === 14) {
+			document.getElementById("subdivision815").style.display = "inline";
+		
+		} else {
 			document.getElementById("subdivision8na").style.display = "inline";
 		}
 
@@ -531,7 +553,252 @@ function createOsc88(beatPerMeasure, beatSound) {											// Function for 8/8 
     beatCount += 1;
 } // end createOsc88()
 
+function createOsc8halfless(beatPerMeasure, beatSound) {									// Function for 9/8,11/8,13/8,15/8 with a subdivision starting with integer<half
+	var gain = audio.createGain();
+	var osc = audio.createOscillator();
 
+	gain.connect(audio.destination);
+    gain.gain.setValueAtTime(0, audio.currentTime);
+    gain.gain.linearRampToValueAtTime(1, audio.currentTime + 1 / 1000);
+    gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
+
+	document.getElementById("overlay").innerHTML = (beatCount % beatPerMeasure) + 1;		// Unlike 4 meter, we display every counted beat making the display logic much simpler
+	
+	if (beatCount === beatPerMeasure) {														// Reset beatCount after each measure
+		beatCount = 0;
+	}
+
+	if (beatSound === 4) {
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 110;
+			osc.type = "sine";
+		} else if (beatCount === Math.floor(beatPerMeasure / 2)) {
+			osc.frequency.value = 220;
+			osc.type = "sawtooth";
+		} else {
+			osc.frequency.value = 880;
+			osc.type = "triangle";
+		}
+
+	} else {
+
+		if (beatSound === 0) {
+			osc.type = "triangle";
+		} else if (beatSound === 1) {
+			osc.type = "sine";
+		} else if (beatSound === 2) {
+			osc.type = "square";
+		} else if (beatSound === 3) {
+			osc.type = "sawtooth";
+		}
+
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 440;
+		} else if (beatCount === Math.floor(beatPerMeasure / 2)) {
+			osc.frequency.value = 330;
+		} else {
+			osc.frequency.value = 220;
+		}
+	}
+
+	osc.connect(gain);
+	osc.start(0);
+
+	setTimeout(function() {
+        osc.stop(0);
+        osc.disconnect(gain);
+       	gain.disconnect(audio.destination);
+    }, 50);
+    beatCount += 1;
+} // end createOsc8halfless()
+
+function createOsc8halfmore(beatPerMeasure, beatSound) {									// Function for 9/8,11/8,13/8,15/8 with a subdivision starting with integer>half
+	var gain = audio.createGain();
+	var osc = audio.createOscillator();
+
+	gain.connect(audio.destination);
+    gain.gain.setValueAtTime(0, audio.currentTime);
+    gain.gain.linearRampToValueAtTime(1, audio.currentTime + 1 / 1000);
+    gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
+
+	document.getElementById("overlay").innerHTML = (beatCount % beatPerMeasure) + 1;		// Unlike 4 meter, we display every counted beat making the display logic much simpler
+	
+	if (beatCount === beatPerMeasure) {														// Reset beatCount after each measure
+		beatCount = 0;
+	}
+
+	if (beatSound === 4) {
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 110;
+			osc.type = "sine";
+		} else if (beatCount === Math.ceil(beatPerMeasure / 2)) {
+			osc.frequency.value = 220;
+			osc.type = "sawtooth";
+		} else {
+			osc.frequency.value = 880;
+			osc.type = "triangle";
+		}
+
+	} else {
+
+		if (beatSound === 0) {
+			osc.type = "triangle";
+		} else if (beatSound === 1) {
+			osc.type = "sine";
+		} else if (beatSound === 2) {
+			osc.type = "square";
+		} else if (beatSound === 3) {
+			osc.type = "sawtooth";
+		}
+
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 440;
+		} else if (beatCount === Math.ceil(beatPerMeasure / 2)) {
+			osc.frequency.value = 330;
+		} else {
+			osc.frequency.value = 220;
+		}
+	}
+
+	osc.connect(gain);
+	osc.start(0);
+
+	setTimeout(function() {
+        osc.stop(0);
+        osc.disconnect(gain);
+       	gain.disconnect(audio.destination);
+    }, 50);
+    beatCount += 1;
+} // end createOsc8halfmore()
+
+function createOsc810(beatPerMeasure, beatSound, accArr) {											// Function for 10/8 in all subdivisions
+	var gain = audio.createGain();
+	var osc = audio.createOscillator();
+
+	gain.connect(audio.destination);
+    gain.gain.setValueAtTime(0, audio.currentTime);
+    gain.gain.linearRampToValueAtTime(1, audio.currentTime + 1 / 1000);
+    gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
+
+	document.getElementById("overlay").innerHTML = (beatCount % beatPerMeasure) + 1;		// Unlike 4 meter, we display every counted beat making the display logic much simpler
+	
+	if (beatCount === beatPerMeasure) {														// Reset beatCount after each measure
+		beatCount = 0;
+	}
+
+	if (beatSound === 4) {
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 110;
+			osc.type = "sine";
+		} else if (beatCount === accArr[0] || beatCount === accArr[1] || beatCount === accArr[2]) {
+			osc.frequency.value = 220;
+			osc.type = "sawtooth";
+		} else {
+			osc.frequency.value = 880;
+			osc.type = "triangle";
+		}
+
+	} else {
+
+		if (beatSound === 0) {
+			osc.type = "triangle";
+		} else if (beatSound === 1) {
+			osc.type = "sine";
+		} else if (beatSound === 2) {
+			osc.type = "square";
+		} else if (beatSound === 3) {
+			osc.type = "sawtooth";
+		}
+
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 440;
+		} else if (beatCount === accArr[0] || beatCount === accArr[1] || beatCount === accArr[2]) {
+			osc.frequency.value = 330;
+		} else {
+			osc.frequency.value = 220;
+		}
+	}
+
+	osc.connect(gain);
+	osc.start(0);
+
+	setTimeout(function() {
+        osc.stop(0);
+        osc.disconnect(gain);
+       	gain.disconnect(audio.destination);
+    }, 50);
+    beatCount += 1;
+} // end createOsc810()
+
+function createOsc814(beatPerMeasure, beatSound, accArr) {											// Function for 14/8 in all subdivisions
+	var gain = audio.createGain();
+	var osc = audio.createOscillator();
+
+	gain.connect(audio.destination);
+    gain.gain.setValueAtTime(0, audio.currentTime);
+    gain.gain.linearRampToValueAtTime(1, audio.currentTime + 1 / 1000);
+    gain.gain.linearRampToValueAtTime(0, audio.currentTime + 50 / 1000);
+
+	document.getElementById("overlay").innerHTML = (beatCount % beatPerMeasure) + 1;		// Unlike 4 meter, we display every counted beat making the display logic much simpler
+	
+	if (beatCount === beatPerMeasure) {														// Reset beatCount after each measure
+		beatCount = 0;
+	}
+
+	if (beatSound === 4) {
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 110;
+			osc.type = "sine";
+		} else if (beatCount === accArr[0] || 
+					beatCount === accArr[1] || 
+					beatCount === accArr[2] || 
+					beatCount === accArr[3] || 
+					beatCount === accArr[4] || 
+					beatCount === accArr[5]) {
+			
+			osc.frequency.value = 220;
+			osc.type = "sawtooth";
+		} else {
+			osc.frequency.value = 880;
+			osc.type = "triangle";
+		}
+
+	} else {
+
+		if (beatSound === 0) {
+			osc.type = "triangle";
+		} else if (beatSound === 1) {
+			osc.type = "sine";
+		} else if (beatSound === 2) {
+			osc.type = "square";
+		} else if (beatSound === 3) {
+			osc.type = "sawtooth";
+		}
+
+		if (beatCount % beatPerMeasure === 0) {
+			osc.frequency.value = 440;
+		} else if (beatCount === accArr[0] || 
+					beatCount === accArr[1] || 
+					beatCount === accArr[2] || 
+					beatCount === accArr[3] || 
+					beatCount === accArr[4] || 
+					beatCount === accArr[5]) {
+			osc.frequency.value = 330;
+		} else {
+			osc.frequency.value = 220;
+		}
+	}
+
+	osc.connect(gain);
+	osc.start(0);
+
+	setTimeout(function() {
+        osc.stop(0);
+        osc.disconnect(gain);
+       	gain.disconnect(audio.destination);
+    }, 50);
+    beatCount += 1;
+} // end createOsc814()
 
 function startMetronome() {																	// Checks user inputs and starts the proper setTimeout function
 	var tempo = document.getElementById("showTempo").innerHTML;
@@ -560,18 +827,60 @@ function startMetronome() {																	// Checks user inputs and starts the
 
 	} else {																				// Logic to determine complex 8 meter based on subdivision and beatPerMeasure
 		beatCount = 0;
+		var accArr;
 
 		if (beatPerMeasure === 4) {
 			interval = setInterval(createOsc84, 60000/tempo, beatPerMeasure, beatSound); 	// Function for 4/8 time signature
 		} else if (beatPerMeasure === 5 && document.getElementById("subdivision85").selectedIndex === 1 ||
 					beatPerMeasure === 7 && document.getElementById("subdivision87").selectedIndex === 1) {
+
 			interval = setInterval(createOsc857, 60000/tempo, beatPerMeasure, beatSound);	// Function for 5/8 time signature with 2 + 3 count and 7/8 with 2 + 2 + 3 count
+		
 		} else if (beatPerMeasure === 7) {
 			interval = setInterval(createOsc87, 60000/tempo, beatPerMeasure, beatSound);	// Function for all other 7/8 subdivisions
-		} else if (beatPerMeasure === 8 && document.getElementById("subdivision88").selectedIndex === 1){
+		
+		} else if (beatPerMeasure === 8 && document.getElementById("subdivision88").selectedIndex === 1) {
 			interval = setInterval(createOsc88, 60000/tempo, beatPerMeasure, beatSound);	// Function for 8/8 with 2 + 3 + 3 subdivision
-		}
-		else {
+		
+		} else if (beatPerMeasure === 9 && document.getElementById("subdivision89").selectedIndex === 1 ||
+					beatPerMeasure === 11 && document.getElementById("subdivision811").selectedIndex === 0 ||
+					beatPerMeasure === 13 && document.getElementById("subdivision813").selectedIndex === 0 ||
+					beatPerMeasure === 15 && document.getElementById("subdivision815").selectedIndex === 1) {
+
+			interval = setInterval(createOsc8halfless, 60000/tempo, beatPerMeasure, beatSound);	// Function for 9/8,11/8,13/8,15/8 with a subdivision starting with integer<half
+		
+		} else if (beatPerMeasure === 9 && document.getElementById("subdivision89").selectedIndex === 2 ||
+					beatPerMeasure === 11 && document.getElementById("subdivision811").selectedIndex === 1 ||
+					beatPerMeasure === 13 && document.getElementById("subdivision813").selectedIndex === 1 ||
+					beatPerMeasure === 15 && document.getElementById("subdivision815").selectedIndex === 2) {
+
+			interval = setInterval(createOsc8halfmore, 60000/tempo, beatPerMeasure, beatSound);	// Function for 9/8,11/8,13/8,15/8 with a subdivision starting with integer>half
+		
+		} else if (beatPerMeasure === 10) {
+			
+			if (document.getElementById("subdivision810").selectedIndex === 0) {			// Logic to determine the accent placement
+				accArr = [ 3, 6, 8 ];
+			} else if (document.getElementById("subdivision810").selectedIndex === 1) {
+				accArr = [ 2, 4, 7 ];
+			} else if (document.getElementById("subdivision810").selectedIndex === 2) {
+				accArr = [ 3, 5, 7 ];
+			} else {
+				accArr = [ 2, 5, 8 ];
+			}
+
+			interval = setInterval(createOsc810, 60000/tempo, beatPerMeasure, beatSound, accArr);	// Function for 10/8 in all subdivisions
+		
+		} else if (beatPerMeasure === 14) {
+			
+			if (document.getElementById("subdivision814").selectedIndex === 0) {			// Logic to determine the accent placement
+				accArr = [ 3, 5, 7, 9, 11 ];
+			} else {
+				accArr = [ 2, 4, 7, 10, 12 ];
+			}
+
+			interval = setInterval(createOsc814, 60000/tempo, beatPerMeasure, beatSound, accArr);	// Function for 14/8 in all subdivisions
+		
+		} else {
 			interval = setInterval(createOsc8, 60000/tempo, beatPerMeasure, beatSound); 	// Function for standard count for all other beatPerMeasure in 8 meter 
 		}
 	}
